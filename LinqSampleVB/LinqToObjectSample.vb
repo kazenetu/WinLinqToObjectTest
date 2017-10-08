6d87caa02fd9641e5628b4d74fd42277fa8adc33
kazenetu <rakudafactory@gmail.com>
2017-10-08 23:05:41 +0900

    GroupJoinのサンプルメソッドを実装

--- LinqSampleVB/LinqToObjectSample.vb
+++ LinqSampleVB/LinqToObjectSample.vb
@@ -25,4 +25,23 @@ Public Class LinqToObjectSample
 
     End Function
 
+    ''' <summary>
+    ''' グループ(外部参照)のサンプル
+    ''' </summary>
+    ''' <param name="src">参照元データ</param>
+    ''' <param name="srcSub">参照元サブデータ</param>
+    ''' <returns>主キーの降順でソートされたデータ</returns>
+    Public Shared Function GroupJoinSample(ByVal src As List(Of Commons.LinqToObjectTest), ByVal srcSub As List(Of Commons.LinqToObjectTest)) As IEnumerable(Of Object)
+
+        Return src.GroupJoin(srcSub, Function(ByVal item As Commons.LinqToObjectTest) item.PrimaryKey,
+            Function(ByVal subItem As Commons.LinqToObjectTest) subItem.ParentPrimaryKey,
+            Function(item, subItems) New With {
+            Key .PrimaryKey = item.PrimaryKey,
+            Key .DispleyOrder = item.DispleyOrder,
+            Key .DataName = item.DataName,
+            Key .existsSubItem = subItems.Any()
+            }).ToList()
+
+    End Function
+
 End Class
