a960d8f573cae16526a259efd58d904039f04b5f
kazenetu <rakudafactory@gmail.com>
2017-10-12 22:52:12 +0900

    C#版「フィルタのサンプル」追加

--- LinqSampleCSharp/LinqToObjectSample.cs
+++ LinqSampleCSharp/LinqToObjectSample.cs
@@ -11,5 +11,14 @@ namespace LinqSampleCSharp
     /// </summary>
     public class LinqToObjectSample
     {
+        /// <summary>
+        /// フィルタのサンプル
+        /// </summary>
+        /// <param name="src">参照元データ</param>
+        /// <returns>主キーが10以上のデータ</returns>
+        public static List<Commons.LinqToObjectTest > WhereSample(List<Commons.LinqToObjectTest> src)
+        {
+            return src.Where((item) => item.PrimaryKey >= 10).ToList();
+        }
     }
 }
